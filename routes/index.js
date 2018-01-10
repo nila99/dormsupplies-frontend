bf447421e241dc69e1160ed7e07e34562b99ea7d
Nila <annadurai@college.harvard.edu>
2018-01-10 17:09:13 -0500

    first commit

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+const request = require('request');
+const config = require('../app/models/config');
+
+router.get('/', (req, res, next) => {
+    return res.render('index');
+});
+
+router.get('/login', (req, res, next) => {
+    return res.render('login');
+});
+
+router.get('/register', (req, res, next) => {
+    return res.render('register');
+});
+
+router.post('/register', (req, res, next) => {
+    request.post({
+    	url:config.apiUrl + '/users',
+    	form: req.body
+    }).pipe(res)
+})
+
+module.exports = router;
